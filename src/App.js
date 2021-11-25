f90e09d0e6f4eacf6c6c1259b077f2bb66e35c78
Kevin Henriquez <kevinkay_2@hotmail.com>
2021-11-24 21:00:38 -0500

    se modifica el index según los mockups

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import icono from './assets/img/icono.png'
 import {Table, Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooter}from 'reactstrap'
 import { render } from '@testing-library/react';
-
+import cultivo from './assets/img/cultivo.jpg'
+import caucho from './assets/img/caucho.jpg'
+import forraje from './assets/img/forraje.jpg'
+import oleoginoso from './assets/img/oleoginoso.jpg'
+import alimentario from './assets/img/alimentario.jpg'
 const data = [
   { id: 1, tipo: "Alimentario", tiempo: "12 meses", terreno: "10 hectareas", producto: "Platano"},
   { id: 2, tipo: "Oleaginoso", tiempo: "9 meses", terreno: "100 hectareas", producto: "Girasol"},
@@ -25,71 +29,65 @@ class App extends React.Component{
   <>
     <Container>
       <header>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <img src={icono} alt="" width="34px" height= "30px" />
-    <a class="navbar-brand" href="#">El Redil</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Login</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Quienes somos</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        <div className="div-Header">
+              <img src={icono} alt="" width="34px" height= "30px" />
+        El Redil S.A.S
+        </div>
       </header>
+      <hr></hr>
     </Container>
     <Container>
-      <br />
-      <Button color="success">Insertar Nuevo Cultivo</Button>
-      <br /><br />
-      <Table>
-        <thead>
-          <tr>
-            <th>
-              Id
-            </th>
-            <th>
-              Tipo
-            </th>
-            <th>
-              Tiempo
-            </th>
-            <th>
-              Area Asignada
-            </th>
-            <th>
-              Producto
-            </th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.data.map((elemento) => (
-            <tr>
-              <td>{elemento.id}</td>
-              <td>{elemento.tipo}</td>
-              <td>{elemento.tiempo}</td>
-              <td>{elemento.terreno}</td>
-              <td>{elemento.producto}</td>
-              <td><Button color="primary">Editar</Button>{"  "}
-              <Button color="danger">eliminar</Button></td>
-            </tr>
-            ))}
-        </tbody>
-      </Table>
+      <div className="div-front">
+        <div className="div-login">
+          <div class="shadow p-3 mb-5 bg-body rounded">
+        iniciar sesion
+          <form>
+            <div class="form-floating mb-3" style={{paddingLeft: "15px", paddingRight: "15px"}}>
+              <input type="text" class="form-control" id="usuario" placeholder="name@example.com"/>
+              <label for="usuario" style={{paddingLeft: "25px"}}>Usuario</label>
+            </div>
+            <div class="form-floating" style={{paddingLeft: "15px", paddingRight: "15px"}}>
+              <input type="password" class="form-control" id="password" placeholder="Password"/>
+              <label for="password" style={{paddingLeft: "25px"}}>Password</label>
+              <button type="button" class="btn btn-primary" style={{ float: 'left', marginTop: '15px', marginRight: '15px',  width: '100%'}}>Ingresar</button>
+              <button type="button" class="btn btn-primary" style={{ marginTop: '15px', width: '100%'}}>Registrate</button>
+              <br></br>
+            <p style={{marginTop: '20px'}}>Olvidaste tu contraseña?</p>
+            </div>
+          </form>
+          </div>
+        </div>
+        <div className="div-texto1">
+          Bienvenidos!
+        </div>
+        <div className="div-textoBienvenida">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."        
+        </div>
+        <div className="div-textoBienvenida">
+          <img src={cultivo} alt="" style={{width: "45%", marginRight: "15px", height: "150px", marginTop: "50px"}}/>
+          <img src={alimentario} alt="" style={{width: "45%", marginRight: "15px", height: "150px", marginTop: "50px"}}/>
+          <img src={caucho} alt="" style={{width: "45%", marginRight: "15px", height: "150px", marginTop: "50px"}}/>
+          <img src={forraje} alt="" style={{width: "45%", marginRight: "15px", height: "150px", marginTop: "50px"}}/>
+        </div>
+      </div>
+    </Container>
+    <Container>
+      <footer>
+        <div className="div-footer">
+          <hr></hr>
+          Dirección: Avenida siempre Viva-123
+          <br></br>
+          Email: elredilsas@elredil.co
+          <br></br>
+          Tel: 035-3465526 
+          <br></br>
+          Cel: 301-4578856
+          <br></br>
+        </div>
+      </footer>
     </Container>
   </>
   );
   }
 }
-
-
 export default App;
